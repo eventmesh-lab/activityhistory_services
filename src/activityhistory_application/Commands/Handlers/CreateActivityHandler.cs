cf6e84b8cb9b68a9ff2e7519e5c32763e71ba315
crismarygonzalez <166092453+crismarygonzalez@users.noreply.github.com>
2026-01-15 04:41:26 -0400

    se agregaron los caso de uso necesarios para el historial de usuario

---
+++ src/activityhistory_application/Commands/Handlers/CreateActivityHandler.cs
@@ -0,0 +1,51 @@
+﻿using activityhistory_application.Commands.Commands;
+using activityhistory_application.DTOs.DTOResponse;
+using activityhistory_application.Interfaces;
+using activityhistory_application.Mappers;
+using activityhistory_domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace activityhistory_application.Commands.Handlers
+{
+    public class CreateActivityHandler: IRequestHandler<CreateActivityCommand, CreateActivityResponseDto>
+    {
+        public readonly IMediator _mediator;
+        public readonly IActivityHistoryRepositoryPostgres _activityHistoryServices;
+        public readonly IUserServices _userServices;
+        public CreateActivityHandler(IActivityHistoryRepositoryPostgres activityHistoryServices, IUserServices userServices)
+        {
+            _activityHistoryServices= activityHistoryServices;
+            _userServices= userServices;
+        }
+        public async Task<CreateActivityResponseDto> Handle(CreateActivityCommand request, CancellationToken cancellationToken)
+        {
+
+            var activity = ActivityHistoryMappers.ToDomain(request);
+
+           
+            try
+            {
+                var usersRegistered = await _userServices.GetIdUserByEmailServices(request.UserEmail);
+                Console.WriteLine($"Usuario obtenido: {usersRegistered}");
+                if (usersRegistered == Guid.Empty) 
+                { 
+                    throw new ApplicationException($"El usuario con email {activity.UserEmail} ya existe en la base de datos.");
+                }
+                await _activityHistoryServices.AddActivity(activity, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+
+                throw new ApplicationException($"No se pudo registar la actividad en la base de datos", ex);
+            }
+            
+            Console.WriteLine("Actividad registrada.");
+            return ActivityHistoryMappers.ToDto(activity);
+        }
+    }
+}
---
+++ src/activityhistory_infrastructure/Migrations/20260115071917_AddMediaColumns.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace activityhistory_infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMediaColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActivityHistories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    Action = table.Column<string>(type: "text", nullable: false),
+                    TimeDate = table.Column<string>(type: "text", nullable: false),
+                    UserEmail = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityHistories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityHistories");
+        }
+    }
+}
